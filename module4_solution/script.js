9374a8ab7254ef856812410f89d0f8c163621a63
Anila VR <anilavr@cs.ajce.in>
2022-05-04 07:29:55 +0530

    week 4

---
+++ module4_solution/script.js
@@ -0,0 +1,17 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for(var i=0;i<(names.length);i++)
+  {
+    var upper=names[i].charAt(0);
+    var lower=upper.toLowerCase();
+    if(upper=='J' || lower=='j')
+    {
+      
+      byeSpeaker.speak(names[i]);
+    }
+    else
+    {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
